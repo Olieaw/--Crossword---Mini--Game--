53b4a71fb7081df2097278dafda47ef8ddda8c64
Kuryakin Danil <olieaw1998@yandex.ru>
2016-04-10 16:55:24 +0300

    добавил код

--- sources/Crossword/App/consoleapp.cpp
+++ sources/Crossword/App/consoleapp.cpp
@@ -3,46 +3,55 @@
 
 void ConsoleApp::Menu()
 {
-    WorkWithVocabulary add;
-    ConsoleApp app;
+    WorkWithVocabulary *add = new WorkWithVocabulary;
     int number_in_menu;
     int word;
 
-    cout << "1. Add word" << endl
-         << "2. Delete word(does not work)" << endl
-         << "0. Exit" << endl
-         << ">>> ";
+    while(1){
+        cout << "1. Add word" << endl
+             << "2. Delete word" << endl
+             << "3. Insert element" <<endl
+             << "0. Exit" << endl
+             << ">>> ";
 
-    cin >> number_in_menu;
-
-    if(cin.good())
-    {
-        switch (number_in_menu)
+        cin >> number_in_menu;
+        if(cin.good())
         {
-        case 0:
-            break;
-        case 1:
-            cout<<"Print word"<<endl;
-            //cin>>word;
-            add.AddLast(1);
-            add.AddLast(2);
-            add.AddLast(3);
-            cout<<add.GetAllItemInfo();
-            app.Menu();
-            break;
-        default:
-            cout << "Something goes wrong!" << endl;
+            switch (number_in_menu)
+            {
+            case 0:
+                return;
+                break;
+            case 1:
+                cout<<"Print word"<<endl;
+                add->AddLast(4353);add->AddLast(542);add->AddLast(9461);add->AddLast(8764);
+                cin>>word;
+                add->AddLast(word);
+                break;
+            case 2:
+                cin>>word;
+                add->Remove(word);
+                break;
+            case 3:
+                cout<< "------------------------" << endl
+                    << endl
+                    << add->GetAllItemInfo()
+                    << endl
+                    << "------------------------" << endl;
+                break;
+            default:
+                cout << "Something goes wrong!" << endl;
+                cin.clear();
+                cout << endl;
+                break;
+            }
+        }
+        else
+        {
+            cout << "Error! Input a number." << endl ;
             cin.clear();
             cout << endl;
             Menu();
-            break;
         }
-    }
-    else
-    {
-        cout << "Error! Input a number." << endl ;
-        cin.clear();
-        cout << endl;
-        Menu();
     }
 }
